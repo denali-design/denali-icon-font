a2607d855da2228c1b2184e93e685aff6d1e94b4
Chas Turansky <chas@turansky.net>
2020-03-24 13:18:29 -0700

    feat: Added new icons (#44)
    Co-authored-by: Chas Turansky <cturansky@oath.com>

--- docs/js/denali-icon-new.js
+++ docs/js/denali-icon-new.js
@@ -2,16 +2,26 @@
 // Licensed under the terms of the MIT license. See LICENSE file in project root for terms.
 
 newIcons = [
-  "exclamation-point-solid",
-  "exclamation-point",
-  "expire-solid",
-  "expire",
-  "medium",
-  "npm",
-  "tree-structure-solid",
-  "tree-structure",
-  "dark-mode-solid",
-  "dark-mode"
+  "battery-charging-solid",
+  "battery-charging",
+  "battery-empty-solid",
+  "battery-empty",
+  "battery-full-solid",
+  "battery-full",
+  "battery-half-solid",
+  "battery-half",
+  "battery-low-solid",
+  "battery-low",
+  "filter-add-solid",
+  "filter-add",
+  "filter-remove-solid",
+  "filter-remove",
+  "search-job-solid",
+  "search-job",
+  "search-money-solid",
+  "search-money",
+  "specs-solid",
+  "specs"
 ];
 
 // updatedIcons = [
