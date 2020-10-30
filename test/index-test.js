1ceb2a271fb96bc1a29f8cbbc36abc42d503f0c4
Chas Turansky <chas@turansky.net>
2020-10-30 11:32:06 -0700

    Update index-test.js

--- test/index-test.js
+++ test/index-test.js
@@ -10,9 +10,9 @@ describe('index', function() {
       assert.match(denali.getFilePath('./'), /\/denali-icon-font$/);
       assert.match(denali.getFilePath('dist/'), /\/denali-icon-font\/dist$/);
       assert.match(denali.getFilePath('dist/denali-icon-font.css'), /denali-icon-font\/dist\/denali-icon-font.css$/);
-      assert.match(denali.getFilePath('build/svg/npm.svg'), /denali-icon-font\/build\/svg\/npm.svg$/);
+      assert.match(denali.getFilePath('build/svg/uEA02-5g.svg'), /denali-icon-font\/build\/svg\/uEA02-5g.svg$/);
 
-      const path = denali.getFilePath('build/svg/npm.svg');
+      const path = denali.getFilePath('build/svg/uEA02-5g.svg');
       assert.match(fs.readFileSync(path, 'utf8'), /<title>npm<\/title>/);
     });
   });
